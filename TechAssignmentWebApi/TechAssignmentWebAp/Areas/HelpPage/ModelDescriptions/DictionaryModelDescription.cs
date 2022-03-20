895ab406bfed15181ff57f8ef04ccbb4d0e4e3d7
unknown <eiphyu.developer.testing@gmail.com>
2022-03-20 10:34:53 +0630

    first commit

---
+++ TechAssignmentWebApi/TechAssignmentWebAp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TechAssignmentWebAp.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TechAssignmentWebApi/TechAssignmentWebAp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TechAssignmentWebAp.WebApiApplication" Language="C#" %>
